dfcfb422a76ce5f70cf7e6d0af495590642d8239
codekongs <531173160@qq.com>
2017-11-30 22:48:06 +0800

    Flask SQLAlchemy学习完成

--- base/hello4.py
+++ base/hello4.py
@@ -3,28 +3,80 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_script import Manager
+from flask_script import Shell
 
+
+'''
+>>> db.create_all()
+>>> from
+>>> from hello4 import Role, User
+>>> admin_role = Role(name='Admin')
+>>> mod_role=Role(name='Moderator')
+>>> user_role=Role(name='User')
+>>> user_john = User(username='john', role=admin_role)
+>>> user_susan = User(username='susan', role=user_role)
+>>> user_david = User(username='david', role=user_role)
+>>> db.session.add(admin_role)
+>>> db.session.add(mod_role)
+>>> db.session.add(user_john)
+>>> db.session.add(user_susan)
+>>> db.session.add(user_david)
+>>> db.session.commit()
+
+# 修改行
+>>> admin_role.name = 'Administrator'
+>>> db.session.add(admin_role)
+>>> db.session.commit()
+
+# 删除行
+>>> db.session.delete(mod_role)
+>>> db.session.commit()
+
+# 查询行
+>>> Role.query.all()
+[<Role 'Administrator'>, <Role 'User'>]
+>>> User.query.all()
+[<User 'john'>, <User 'susan'>, <User 'david'>]
+>>> User.query.filter_by(role=user_role).all()
+[<User 'susan'>, <User 'david'>]
+>>> str(User.query.filter_by(role=user_role).all())
+"[<User 'susan'>, <User 'david'>]"
+>>> str(User.query.filter_by(role=user_role))
+'SELECT users.id AS users_id, users.username AS users_username,
+users.role_id AS users_role_id \nFROM users \nWHERE ? = users.role_id'
+
+'''
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, 'data.sqlite')
+manager = Manager(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+
 class Role(db.Model):
-    __table__ = 'roles'
+    __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relaionship('User', backref='role')
+    users = db.relationship('User', backref='role')
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
 
 class User(db.Model):
-    __table__ = 'users'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
