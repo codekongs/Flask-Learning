82a91cb02445e720527e8da63431803797012e94
codekongs <531173160@qq.com>
2017-11-30 12:19:20 +0800

    学习Flask的表单

---
+++ base/hello3.py
@@ -0,0 +1,53 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import render_template, session, redirect, url_for, flash
+from datetime import datetime
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+
+class NameForm(Form):
+    name = StringField('姓名', validators=[Required()])
+    submit = SubmitField('提交')
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('你貌似改变了你的名字')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=datetime.utcnow(),
+                           form=form, name=session.get('name'))
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
